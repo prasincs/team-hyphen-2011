57e40f3d427f3cb7be88ca4f8572725902191db3
prasincs <prasannagautam@gmail.com>
2011-08-26 20:19:08 -0400

    added stuff

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // server.js 
 var http = require('http') 
-, nko = require('nko')('3K5CfNDu8AAVXRy3');
 
 var app = http.createServer(function (req, res) { 
 res.writeHead(200, { 'Content-Type': 'text/html' }); 
